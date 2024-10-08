c252a20f6e08728c578a38410f68f3609e674585
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-08 22:56:16 +0500

    feat: tasks styles changed

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,18 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent]
 })
-
 export class TasksComponent {
   @Input() name?: string;
-
-
-  // get UserName() {
-  //   return `assets/users/${this.name}`;
-  // }
 }
